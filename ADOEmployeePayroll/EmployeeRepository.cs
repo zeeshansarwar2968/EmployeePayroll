6c7345a0c6ffc4aedd6b7d1e47a4cac3757198b9
zeeshansarwar2968 <zeeshansarwar2968@gmail.com>
2022-06-20 20:41:53 +0530

    [Zeeshan] : Added logic to retrieve data

--- ADOEmployeePayroll/EmployeeRepository.cs
+++ ADOEmployeePayroll/EmployeeRepository.cs
@@ -9,9 +9,51 @@ namespace ADOEmployeePayroll
 {
     class EmployeeRepository
     {
+        
         //Give path for Database Connection
-        public static string connection = @"Server=.;Database=EmployeeServices;Trusted_Connection=True;";
+        public static string connection = @"data source=LAPTOP-ICIBRBKA\MSSQLSERVER01; database=payroll_service; integrated security=true;";
         //Represents a connection to Sql Server Database
         SqlConnection sqlConnection = new SqlConnection(connection);
+
+        //Create Object for EmployeeData Repository
+        EmployeeDataManager employeeDataManager = new EmployeeDataManager();
+        public void GetSqlData()
+        {
+            //Open Connection
+            sqlConnection.Open();   
+            string query = "select * from employee_payroll";
+            //Pass query to TSql
+            SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+            SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+            //Check if swlDataReader has Rows
+            if (sqlDataReader.HasRows)
+            {
+                //Read each row
+                while (sqlDataReader.Read())
+                {
+                    //Read data SqlDataReader and store 
+                    employeeDataManager.EmployeeID = Convert.ToInt32(sqlDataReader["id"]);
+                    employeeDataManager.EmployeeName = sqlDataReader["name"].ToString();
+                    employeeDataManager.BasicPay = Convert.ToDouble(sqlDataReader["BasicPay"]);
+                    employeeDataManager.Deduction = Convert.ToDouble(sqlDataReader["Deduction"]);
+                    employeeDataManager.IncomeTax = Convert.ToDouble(sqlDataReader["IncomeTax"]);
+                    employeeDataManager.TaxablePay = Convert.ToDouble(sqlDataReader["TaxablePay"]);
+                    employeeDataManager.NetPay = Convert.ToDouble(sqlDataReader["NetPay"]);
+                    employeeDataManager.Gender = Convert.ToChar(sqlDataReader["Gender"]);
+                    employeeDataManager.EmployeePhoneNumber = Convert.ToInt64(sqlDataReader["EmployeePhoneNumber"]);
+                    employeeDataManager.EmployeeDepartment = sqlDataReader["EmployeeDepartment"].ToString();
+                    employeeDataManager.Address = sqlDataReader["Address"].ToString();
+                    employeeDataManager.StartDate = Convert.ToDateTime(sqlDataReader["startDate"]);
+
+                    //Display Data
+                    Console.WriteLine("\nEmployee ID: {0} \t Employee Name: {1} \nBasic Pay: {2} \t Deduction: {3} \t Income Tax: {4} \t Taxable Pay: {5} \t NetPay: {6} \nGender: {7} \t PhoneNumber: {8} \t Department: {9} \t Address: {10}", employeeDataManager.EmployeeID, employeeDataManager.EmployeeName, employeeDataManager.BasicPay, employeeDataManager.Deduction, employeeDataManager.IncomeTax, employeeDataManager.TaxablePay, employeeDataManager.NetPay, employeeDataManager.Gender, employeeDataManager.EmployeePhoneNumber, employeeDataManager.EmployeeDepartment, employeeDataManager.Address);
+                }
+                //Close sqlDataReader Connection
+                sqlDataReader.Close();
+            }
+            //Close Connection
+            sqlConnection.Close();
+        }
     }
 }
