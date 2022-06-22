64f3f2d100cf623283ebf8b5bdff5d22f72ab42f
zeeshansarwar2968 <zeeshansarwar2968@gmail.com>
2022-06-22 09:58:47 +0530

    [Zeeshan] : Refactored logic to update data using stored procedures

--- ADOEmployeePayroll/EmployeeRepository.cs
+++ ADOEmployeePayroll/EmployeeRepository.cs
@@ -4,6 +4,7 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 
 namespace ADOEmployeePayroll
 {
@@ -24,9 +25,10 @@ namespace ADOEmployeePayroll
             string query = "select * from employee_payroll";
             //Pass query to TSql
             SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+            //sqldatareader provides a way of reading forward-only stream of rows of data
             SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
 
-            //Check if swlDataReader has Rows
+            //Check if sqlDataReader has Rows
             if (sqlDataReader.HasRows)
             {
                 //Read each row
@@ -52,12 +54,12 @@ namespace ADOEmployeePayroll
                 //Close sqlDataReader Connection
                 sqlDataReader.Close();
             }
-            //Close Connection
+            //Close the Connection
             sqlConnection.Close();
         }
 
         //Logic to update the salary to 3000000 as per 3rd UseCase
-        public void UpdateSalaryQuery()
+        public int UpdateSalaryQuery()
         {
             //Open Connection
             sqlConnection.Open();
@@ -76,6 +78,141 @@ namespace ADOEmployeePayroll
             //Close Connection
             sqlConnection.Close();
             GetSqlData();
+            return result;
+        }
+
+        public bool AddEmployee(EmployeeDataManager employeeDataManager)
+        {
+            try
+            {
+                using (sqlConnection)
+                {
+                    SqlCommand sqlCommand = new SqlCommand("spAddEmployee", sqlConnection);
+                    sqlCommand.CommandType = CommandType.StoredProcedure;
+                    sqlCommand.Parameters.AddWithValue("@name", employeeDataManager.EmployeeName);
+                    sqlCommand.Parameters.AddWithValue("@PhoneNumber", employeeDataManager.EmployeePhoneNumber);
+                    sqlCommand.Parameters.AddWithValue("@Address", employeeDataManager.Address);
+                    sqlCommand.Parameters.AddWithValue("@Department", employeeDataManager.EmployeeDepartment);
+                    sqlCommand.Parameters.AddWithValue("@Gender", employeeDataManager.Gender);
+                    sqlCommand.Parameters.AddWithValue("@BasicPay", employeeDataManager.BasicPay);
+                    sqlCommand.Parameters.AddWithValue("@Deductions", employeeDataManager.Deduction);
+                    sqlCommand.Parameters.AddWithValue("@TaxablePay", employeeDataManager.TaxablePay);
+                    sqlCommand.Parameters.AddWithValue("@Tax", employeeDataManager.IncomeTax);
+                    sqlCommand.Parameters.AddWithValue("@NetPay", employeeDataManager.NetPay);
+                    sqlCommand.Parameters.AddWithValue("@StartDate", DateTime.Now);
+
+                    //Opens connection thread to the database
+                    sqlConnection.Open();
+
+                    //Executing the queries, here we are executing the stored procedure
+                    var result = sqlCommand.ExecuteNonQuery();
+
+                    //Closes the connection to database
+                    sqlConnection.Close();
+
+                    if (result != 0)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+        }
+
+        public bool UpdateSalary(EmployeeDataManager employeeDataManager)
+        {
+            int result = 0;
+            try
+            {
+                using (sqlConnection)
+                {
+                    //Give stored Procedure
+                    SqlCommand sqlCommand = new SqlCommand("dbo.spUpdateSalaryF", this.sqlConnection);
+                    sqlCommand.CommandType = CommandType.StoredProcedure;
+                    sqlCommand.Parameters.AddWithValue("@salary", employeeDataManager.BasicPay);
+                    sqlCommand.Parameters.AddWithValue("@name", employeeDataManager.EmployeeName);
+                    sqlCommand.Parameters.AddWithValue("@EmpId", employeeDataManager.EmployeeID);
+                    //Open Connection
+                    sqlConnection.Open();
+                    //Return Number of Rows affected
+                    result = sqlCommand.ExecuteNonQuery();
+                    if (result != 0)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return false;
+        }
+        public int RetrieveQuery(string name)
+        {
+
+            int result = 0;
+            try
+            {
+                using (sqlConnection)
+                {
+                    //Give stored Procedure
+                    SqlCommand sqlCommand = new SqlCommand("dbo.spRetrieveDataUsingName", this.sqlConnection);
+                    sqlCommand.CommandType = CommandType.StoredProcedure;
+                    sqlCommand.Parameters.AddWithValue("@name", name);
+                    //Open Connection
+                    sqlConnection.Open();
+                    SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+                    //Check if swlDataReader has Rows
+                    if (sqlDataReader.HasRows)
+                    {
+                        //Read each row
+                        while (sqlDataReader.Read())
+                        {
+                            result++;
+                            //Read data SqlDataReader and store 
+                            employeeDataManager.EmployeeID = Convert.ToInt32(sqlDataReader["id"]);
+                            employeeDataManager.EmployeeName = sqlDataReader["name"].ToString();
+                            employeeDataManager.BasicPay = Convert.ToDouble(sqlDataReader["BasicPay"]);
+                            employeeDataManager.Deduction = Convert.ToDouble(sqlDataReader["Deduction"]);
+                            employeeDataManager.IncomeTax = Convert.ToDouble(sqlDataReader["IncomeTax"]);
+                            employeeDataManager.TaxablePay = Convert.ToDouble(sqlDataReader["TaxablePay"]);
+                            employeeDataManager.NetPay = Convert.ToDouble(sqlDataReader["NetPay"]);
+                            employeeDataManager.Gender = Convert.ToChar(sqlDataReader["Gender"]);
+                            employeeDataManager.EmployeePhoneNumber = Convert.ToInt64(sqlDataReader["EmployeePhoneNumber"]);
+                            employeeDataManager.EmployeeDepartment = sqlDataReader["EmployeeDepartment"].ToString();
+                            employeeDataManager.Address = sqlDataReader["Address"].ToString();
+                            employeeDataManager.StartDate = Convert.ToDateTime(sqlDataReader["StartDate"]);
+
+                            //Display Data
+                            Console.WriteLine("\nEmployee ID: {0} \t Employee Name: {1} \nBasic Pay: {2} \t Deduction: {3} \t Income Tax: {4} \t Taxable Pay: {5} \t NetPay: {6} \nGender: {7} \t PhoneNumber: {8} \t Department: {9} \t Address: {10}", employeeDataManager.EmployeeID, employeeDataManager.EmployeeName, employeeDataManager.BasicPay, employeeDataManager.Deduction, employeeDataManager.IncomeTax, employeeDataManager.TaxablePay, employeeDataManager.NetPay, employeeDataManager.Gender, employeeDataManager.EmployeePhoneNumber, employeeDataManager.EmployeeDepartment, employeeDataManager.Address);
+                        }
+                        //Close sqlDataReader Connection
+                        sqlDataReader.Close();
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            sqlConnection.Close();
+            return result;
         }
     }
 }
--- ADOEmployeePayroll/Program.cs
+++ ADOEmployeePayroll/Program.cs
@@ -6,9 +6,107 @@ namespace ADOEmployeePayroll
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Payroll Application using ADO.net");
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("\t\t\t-----------------------------------------------------------");
+            Console.WriteLine("\t\t\t   Welcome to Employee Payroll Application using ADO.net");
+            Console.WriteLine("\t\t\t-----------------------------------------------------------");
+            Console.ResetColor();
+            Console.WriteLine("");
             EmployeeRepository employeeRepository = new EmployeeRepository();
-            employeeRepository.UpdateSalaryQuery();
+            
+            //employeeRepository.UpdateSalaryQuery();
+            //employeeRepository.UpdateSalary("Sarwar");
+            Console.WriteLine("1. Get Employee Data from the available Database");
+            Console.WriteLine("2. Add New Employee to the present database");
+            Console.WriteLine("3. Update Employee data in the database");
+            Console.WriteLine("4. Get details Employee from database using Employee Name");
+            Console.ForegroundColor= ConsoleColor.Red;
+            Console.WriteLine("Press 0 to Exit");
+            Console.ResetColor();
+            Console.WriteLine("");
+            Console.Write("Enter from the available options to start program execution : ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    employeeRepository.GetSqlData();
+                    break;
+                case 2:
+                    AddEmployeeData();
+                    employeeRepository.GetSqlData();
+                    break;
+                case 3:
+                    UpdateQuery();
+                    employeeRepository.GetSqlData();
+                    break;
+                case 4:
+                    employeeRepository.RetrieveQuery("Fineas");
+                    break;
+                case 0:
+                    return;
+                default:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Please enter a valid choice from the above options!!");
+                    Console.ResetColor();
+                    break;
+            }
+         
         }
+
+        //Function/method to add rows of data to the connected database
+        public static void AddEmployeeData()
+        {
+            EmployeeDataManager employeeDataManager = new EmployeeDataManager();
+
+            employeeDataManager.EmployeeName = "Fineas";
+            employeeDataManager.Gender = Convert.ToChar("M");
+            employeeDataManager.EmployeePhoneNumber = 7654189765;
+            employeeDataManager.EmployeeDepartment = "IT";
+            employeeDataManager.Address = "56/1, Bolevyard Street, NY";
+            employeeDataManager.BasicPay = 75000;
+            employeeDataManager.Deduction = 1500;
+            employeeDataManager.TaxablePay = 1000;
+            employeeDataManager.IncomeTax = 5000;
+            employeeDataManager.NetPay = 70000;
+            employeeDataManager.StartDate = Convert.ToDateTime("2020-09-06");
+
+            EmployeeRepository employeeRepository = new EmployeeRepository();
+
+            bool result = employeeRepository.AddEmployee(employeeDataManager);
+
+            if (result == true)
+            {
+                Console.WriteLine("Data is inserted into database");
+            }
+            else
+            {
+                Console.WriteLine("Data is not inserted into database");
+            }
+        }
+
+
+        //Function/method to update present data in the connected database
+        public static void UpdateQuery()
+        {
+            EmployeeDataManager employeeDataManager = new EmployeeDataManager();
+
+            employeeDataManager.EmployeeID = 6;
+            employeeDataManager.BasicPay = 120000;
+            employeeDataManager.EmployeeName = "Fineas";
+
+            EmployeeRepository employeeRepository = new EmployeeRepository();
+
+            bool result = employeeRepository.UpdateSalary(employeeDataManager);
+
+            if (result == true)
+            {
+                Console.WriteLine("Data is updated in the database");
+            }
+            else
+            {
+                Console.WriteLine("Data is not updated in the database");
+            }            
+        }
+
     }
 }
